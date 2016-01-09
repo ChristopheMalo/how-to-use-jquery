d14acaf7cef8da87d351658fef8501292d01ad5e
ChristopheMalo <christof.malo@gmail.com>
2016-01-09 20:01:06 +0100

    Coder exemples cours 05 -> Remplacer des éléments

--- js/code_js_cours_05_inserer_remplacer_elements_dom.js
+++ js/code_js_cours_05_inserer_remplacer_elements_dom.js
@@ -7,10 +7,10 @@
  * Insérer du contenu
  */
 // Ajout d'une espace et de trois astérisques à la suite de chaque titre <h2>
-$('h2').append(' ***');
+$('h3').append(' ***');
 
 // Ajout de trois astérisques et d'une espace avant chaque titre <h2>
-$('h2').prepend('*** ');
+$('h3').prepend('*** ');
 
 // Ajout d'une ligne de séparation horizontale avant le titre <h2> #trois
 $('#trois').before('<hr>');
@@ -21,3 +21,18 @@ $('hr').css('border-color', '#bbb');
 // insertion elements après balise selector
 // $(selector).after(things-to-add);
 $('hr').after('<p>Test insert after</p>');
+
+/*
+ * Remplacer des éléments
+ */
+// Remplacer hr par saut de ligne
+$('hr').replaceWith('<br>');
+
+// Remplacer h3 par des h4
+//$('h3').replaceWith('<h4>'); // Remplace simplement la balise, tout le contenu est perdu
+// La bonne methode
+$('h3').each(function() {
+   var elementH3 = $(this);
+   elementH3.replaceWith('<h4>' + elementH3.text() + '</h4>');
+});
+
