2702e8b9761b88bd53c833b13ebcf061d2edb92b
ChristopheMalo <christof.malo@gmail.com>
2016-01-10 11:16:16 +0100

    Coder exemples cours 05 -> Insérer des éléments

--- js/code_js_cours_05_inserer_remplacer_elements_dom.js
+++ js/code_js_cours_05_inserer_remplacer_elements_dom.js
@@ -3,36 +3,53 @@
  * 
  * 
  */
-/*
- * Insérer du contenu
- */
-// Ajout d'une espace et de trois astérisques à la suite de chaque titre <h2>
-$('h3').append(' ***');
+$(function() {
+ 
+    /*
+     * Insérer du contenu
+     */
+    // Ajout d'une espace et de trois astérisques à la suite de chaque titre <h2>
+    $('h3').append(' ***');
 
-// Ajout de trois astérisques et d'une espace avant chaque titre <h2>
-$('h3').prepend('*** ');
+    // Ajout de trois astérisques et d'une espace avant chaque titre <h2>
+    $('h3').prepend('*** ');
 
-// Ajout d'une ligne de séparation horizontale avant le titre <h2> #trois
-$('#trois').before('<hr>');
+    // Ajout d'une ligne de séparation horizontale avant le titre <h2> #trois
+    $('#trois').before('<hr>');
 
-// Modifier les caracteristiques du hr
-$('hr').css('border-color', '#bbb');
+    // Modifier les caracteristiques du hr
+    $('hr').css('border-color', '#bbb');
 
-// insertion elements après balise selector
-// $(selector).after(things-to-add);
-$('hr').after('<p>Test insert after</p>');
+    // insertion elements après balise selector
+    // $(selector).after(things-to-add);
+    $('hr').after('<p>Test insert after</p>');
 
-/*
- * Remplacer des éléments
- */
-// Remplacer hr par saut de ligne
-$('hr').replaceWith('<br>');
+    /*
+     * Remplacer des éléments
+     */
+    // Remplacer hr par saut de ligne
+    $('hr').replaceWith('<br>');
 
-// Remplacer h3 par des h4
-//$('h3').replaceWith('<h4>'); // Remplace simplement la balise, tout le contenu est perdu
-// La bonne methode
-$('h3').each(function() {
-   var elementH3 = $(this);
-   elementH3.replaceWith('<h4>' + elementH3.text() + '</h4>');
+    // Remplacer h3 par des h4
+    //$('h3').replaceWith('<h4>'); // Remplace simplement la balise, tout le contenu est perdu
+    // La bonne methode
+    $('h3').each(function() {
+       var elementH3 = $(this);
+       elementH3.replaceWith('<h4>' + elementH3.text() + '</h4>');
+    });
+    
+    /*
+     * Insérer des éléments
+     */
+    // Ajout d'un élément de liste à puces après le deuxième élément
+    $('<li>Deuxième élément bis</li>').insertAfter($('li:nth-child(2)'));
+    
+    // Ajout d'une balise <hr> avant chaque titre <h4>
+    $('<hr>').prependTo($('h4'));
+    $('hr').css('border-color', '#bbb');
+    
+    // Juste pour l'exemple car HTML incorrect - Incohérence sémantique
+    // Insère une puce dans une autre puce - avant le texte
+    $('<li>Deuxième élément ter</li>').prependTo($('li:nth-child(2)'));
+
 });
-
