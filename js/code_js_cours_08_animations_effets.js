2e21dbc4371ed0149bd8f2790a4a3b2563468715
ChristopheMalo <christof.malo@gmail.com>
2016-01-15 22:55:56 +0100

    Coder exemples cours 08 -> Apparition et disparition

--- js/code_js_cours_08_animations_effets.js
+++ js/code_js_cours_08_animations_effets.js
@@ -3,3 +3,28 @@
  * 
  * 
  */
+/*
+ * Apparition et disparition
+ */
+$(function() {
+    $('td').css('text-align','center'); // Centrer le texte dans la colonne
+    
+    $('#affiche').click(function() {
+        $('tr:even').show('slow');
+    } ); 
+    $('#cache').click(function() {
+        $('tr:even').hide(1000);
+    });
+    
+    // en cascade
+    $('#affiche2').click(function() {
+        $('img').first().show('slow', function showNextOne() {
+            $(this).next('img').show('slow', showNextOne);
+        });    
+    });
+    $('#cache2').click(function() {
+        $('img').first().hide('slow', function hideNextOne() {
+            $(this).next('img').hide('slow', hideNextOne);
+        });    
+    });
+});
