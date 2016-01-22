c7a15df34b17825b6a88c96caa118f5a6ce29014
ChristopheMalo <christof.malo@gmail.com>
2016-01-22 20:43:06 +0100

    Coder exemples cours 10 -> Position d'un caractère

--- js/code_js_cours_10_textes_images.js
+++ js/code_js_cours_10_textes_images.js
@@ -6,11 +6,19 @@
 /*
  * Las chaines de caractères
  */
-// Supprimer des espaces dans une chaine
 $(function() {
+    // Supprimer des espaces dans une chaine
     $('#action').click(function() {
       var texte = $('#texte').val();
       $('#resultat').html('Texte original           : "' + texte + '"' + 
                       '<br>Après la fonction trim() : "' + $.trim(texte) + '"');
+    });
+    
+    // Position d'un caractère dans une chaine
+    $('#action2').click(function() {
+        var texte = $('#texte2').val();
+        var charPosition = $('#position').val();
+        var resultat = 'Le caractère en position ' + charPosition + ' est un "' + texte.charAt(charPosition) + '"';
+        $('#resultat2').text(resultat);
     });  
 });
