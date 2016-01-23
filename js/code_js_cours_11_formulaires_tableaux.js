28d301433283c774d44ea9132e1b6bf922b12de9
ChristopheMalo <christof.malo@gmail.com>
2016-01-23 14:21:57 +0100

    Coder exemples cours 10 -> Formulaires : déterminer focus et modifier CSS du focus

--- js/code_js_cours_11_formulaires_tableaux.js
+++ js/code_js_cours_11_formulaires_tableaux.js
@@ -11,9 +11,22 @@ $(function() {
     $('#focus').focus();
     
     // Déterminer le focus
+//    var focusIs;  
+//    $('#determiner-focus input, #determiner-focus textarea').focus( function() { 
+//        focusIs = $(this).attr('id');        
+//        $('#resultat1').text(focusIs + ' a le focus');
+//    });
+    
+    // Déterminer le focus et modifer le CSS du focus
     var focusIs;  
     $('#determiner-focus input, #determiner-focus textarea').focus( function() { 
-        focusIs = $(this).attr('id');        
+        focusIs = '#' + $(this).attr('id');
         $('#resultat1').text(focusIs + ' a le focus');
+        $(focusIs).css('background-color', '#afc');
     }); 
+    $('#determiner-focus input, #determiner-focus textarea').blur( function() { 
+        focusIs = '#' + $(this).attr('id');        
+        $(focusIs).css('background-color', '#fff');
+    }); 
+
 });
