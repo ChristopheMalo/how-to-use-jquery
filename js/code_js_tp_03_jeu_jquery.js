f8d0313f34e6e95b281c8f5a55f6204c5635472e
ChristopheMalo <christof.malo@gmail.com>
2016-01-27 20:54:38 +0100

    Coder le déplacement de la voiture jaune TP 03

--- js/code_js_tp_03_jeu_jquery.js
+++ js/code_js_tp_03_jeu_jquery.js
@@ -7,26 +7,40 @@ $(function() {
  
     function deplace() {
         // Afficher et animer la voiture rouge
-        $('#vr').animate({top: '-=600'}, 2500, 'linear', function(){
+        $('#vr').animate({top: '-=600'}, 2500, 'linear', function() {
             var vrX = Math.floor(Math.random()*194)+70;
             var vrY = 400;
             $('#vr').css('top',vrY);
             $('#vr').css('left',vrX);
         });
-
-        
+         
         // Fonction de déplacement de la route
-        $('.fond').animate({
-            top: '-=360'
-        },
-        1000,
-        'linear',
-        function(){
+        $('.fond').animate( {top: '-=360' }, 1000, 'linear', function(){
             $('.fond').css('top', 0);
             deplace();
         });
-    }
+    };
     
-    deplace(); // Déplacement de la route
+    // Déplacer la voiture jaune
+    $(document).keydown(function(e){
+        if (e.which === 39) {
+            vjX = parseInt($('#vj').css('left'));
+            
+            if (vjX < 280) {
+                $('#vj').css('left', vjX + 30);
+            }
+        }
+        
+        if (e.which === 37) {
+            vjX = parseInt($('#vj').css('left'));
+          
+            if (vjX > 70) {
+                $('#vj').css('left', vjX - 30);
+            }
+        }
+    });
+    
+    
+    deplace(); // Appel de la fonction pour le déplacement de la route et de la voiture rouge
     
 });
