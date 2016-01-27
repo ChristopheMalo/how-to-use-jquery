b5e5ac9e97ca98715cdc4f4b55953f83a3d93ec6
ChristopheMalo <christof.malo@gmail.com>
2016-01-27 18:46:57 +0100

    Coder le déplacement de la route haut vers bas TP 03

--- js/code_js_tp_03_jeu_jquery.js
+++ js/code_js_tp_03_jeu_jquery.js
@@ -3,3 +3,19 @@
  * 
  * 
  */
+$(function() {
+    // Fonction de déplacement de la route
+    function deplace() {
+        $('.fond').animate({
+            top: '-=360'
+        },
+        1000,
+        'linear',
+        function(){
+            $('.fond').css('top', 0);
+            deplace();
+        });
+    }
+    
+    deplace(); // Déplacement de la route
+});
