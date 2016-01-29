534d85715839352850c96cc4b2fd359cc3ed1973
ChristopheMalo <christof.malo@gmail.com>
2016-01-29 19:17:22 +0100

    Coder le déplacement de la soucoupe TP 04

--- js/code_js_tp_04_jeu_collecte_spatiale.js
+++ js/code_js_tp_04_jeu_collecte_spatiale.js
@@ -4,5 +4,75 @@
  * 
  */
 $(function() {
-    
+    // Déplacement de la soucoupe sur l'aire de jeu...
+    $(document).keydown(function(e){
+        // ...vers la droite
+        if (e.which === 39) {
+            posX = parseInt($('#soucoupe').css('left'));
+            if (posX < 460) {
+                $('#soucoupe').css('left', posX+30);
+            }
+        }
+        
+        // ...vers la gauche
+        if (e.which === 37) {
+            posX = parseInt($('#soucoupe').css('left'));
+            if (posX > 20) {
+                $('#soucoupe').css('left', posX-30);
+            }
+        }
+        
+        // ...vers le bas
+        if (e.which === 40) {
+            posY = parseInt($('#soucoupe').css('top'));
+            if (posY < 230) {
+                $('#soucoupe').css('top', posY+30);
+            }
+        }
+        
+        // ...vers le haut
+        if (e.which === 38) {
+            posY = parseInt($('#soucoupe').css('top'));
+            if (posY > 20) {
+                $('#soucoupe').css('top', posY-30);
+            }
+        } 
+        
+        // ...vers le haut et la gauche
+        if (e.which === 36) {
+            posX = parseInt($('#soucoupe').css('left'));
+            posY = parseInt($('#soucoupe').css('top'));
+            if ((posY > 20) && (posX > 20)) {
+                $('#soucoupe').css('left', posX-30).css('top', posY-30);
+            }
+        }
+        
+        // ...vers le haut et la droite
+        if (e.which === 33) {
+            posX = parseInt($('#soucoupe').css('left'));
+            posY = parseInt($('#soucoupe').css('top'));
+            if ((posY > 20) && (posX < 460)) {
+                $('#soucoupe').css('left', posX+30).css('top', posY-30);
+            }
+        }
+        
+        // ...vers le bas et la gauche
+        if (e.which === 35) {
+            posX = parseInt($('#soucoupe').css('left'));
+            posY = parseInt($('#soucoupe').css('top'));
+            if ((posX > 20) && (posY < 230)) {
+                $('#soucoupe').css('left', posX-30).css('top', posY+30);
+            }
+        }
+        
+        // ...vers le bas et la droite
+        if (e.which === 34) {
+            posX = parseInt($('#soucoupe').css('left'));
+            posY = parseInt($('#soucoupe').css('top'));
+            if ((posY < 230) && (posX < 460)) {
+                $('#soucoupe').css('left', posX+30).css('top', posY+30);
+            }
+        }
+    });
+
 });
