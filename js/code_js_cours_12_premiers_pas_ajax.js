7b1df4e43aabf6d454ba9c6444f0a6ab616da4e3
ChristopheMalo <christof.malo@gmail.com>
2016-01-30 17:18:17 +0100

    Coder exemples cours 12 -> GET et POST

--- js/code_js_cours_12_premiers_pas_ajax.js
+++ js/code_js_cours_12_premiers_pas_ajax.js
@@ -20,12 +20,14 @@ $(function() {
         });
     });
     
+    
     // Charger une partie d'un fichier
     $('#majPremierB').click(function() {
         $('#premier').load('../files/cours_12/maj1.html #modifMaj1b', function() {
             console.log('La première zone a été mise à jour avec une partie du fichier');
         });
     });
+    
     
     // Passer des paramètres à un programme PHP
     // 1ère forme
@@ -38,5 +40,20 @@ $(function() {
     // 2ème forme - en passant un objet en 2ème argument de la méthode load
     // Valeur passée par une requête http POST donc les données doivent être récupérées en POST
     // $('sel').load('http://www.url-site.com/page-test.php', { id:20, nom: 'Toto'});
+    
+    
+    // Requêtes GET et POST
+    // Fonction $.GET
+    $('#action2').click(function() {
+        $.get('../files/cours_12/proverbes.php?l=9', function(data) {
+            console.log(data);
+        });    
+    });
+    
+    // Fonction $.POST - exemple
+//    $.post('traiteFormulaire.php', { nom: 'Pierre34', heure: '2pm', post:'Un peu de texte récupéré dans un formulaire HTML et destiné à être posté dans un forum.' },
+//        function(data) {
+//            alert(data);
+//    });
 
 });
