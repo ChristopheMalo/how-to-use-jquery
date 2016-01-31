b7c25ecf6fe1d57e0b8761b011f80044e13aa7f5
ChristopheMalo <christof.malo@gmail.com>
2016-01-31 01:05:20 +0100

    Coder exemples cours 13 -> Évènements associés Ajax

--- js/code_js_cours_13_ajax_avancee.js
+++ js/code_js_cours_13_ajax_avancee.js
@@ -34,7 +34,60 @@ $(function() {
             error: function() {
                 console.log('La requête n\'a pas abouti'); }
         });    
+    }); 
+    
+    
+    // Attention depuis la version 1.8 les méthodes .ajax sont attachés uniquement à document
+    // L'exemple OC doit donc être adapté
+    // Évènements associés à une requête Ajax
+    $('#action2').click(function() {
+        // À partir de la version 1.8
+        $(document).ajaxStart(function() {
+            $('#message').html('Méthode ajaxStart exécutée<br>'); 
+        });
+        $(document).ajaxSend(function(event, request, options) {
+            $('#message').append('Méthode ajaxSend exécutée, ');
+            $('#message').append('nom du fichier : ' + options.url + '<br>'); 
+        });
+        $(document).ajaxStop(function() {
+            $('#message').append('Méthode ajaxStop exécutée<br>');
+        });
+        $(document).ajaxSuccess(function(event, request, options) {
+            $('#message').append('Méthode ajaxSuccess exécutée<br>');
+        });
+        $(document).ajaxComplete(function(event, request, options) {
+            $('#message').append('Méthode ajaxComplete exécutée<br>');
+        });
+        $(document).ajaxError(function(event, request, options, erreur) {
+            $('#message').append('Méthode ajaxError exécutée, ');
+            $('#message').append('erreur : ' + erreur + '<br>');
+        });
+        $('#donnees').load('../files/cours_13/file.html');
+        
+        // Avant la version 1.8
+//        $('#message').ajaxStart(function() {
+//            $(this).html('Méthode ajaxStart exécutée<br>');
+//        });
+//        $('#message').ajaxSend(function(event, request, options) {
+//            $(this).append('Méthode ajaxSend exécutée, ');
+//            $(this).append('nom du fichier : ' + options.url + '<br>');
+//        });
+//        $('#message').ajaxStop(function() {
+//            $(this).append('Méthode ajaxStop exécutée<br>');
+//        });
+//        $('#message').ajaxSuccess(function(event, request, options) {
+//            $(this).append('Méthode ajaxSuccess exécutée<br>');
+//        });
+//        $('#message').ajaxComplete(function(event, request, options) {
+//            $(this).append('Méthode ajaxComplete exécutée<br>');
+//        });
+//        $('#message').ajaxError(function(event, request, options, erreur) {
+//            $(this).append('Méthode ajaxError exécutée, ');
+//            $(this).append('erreur : ' + erreur + '<br>');
+//        });
+//        $('#donnees').load('../files/cours_13/file.html');
     });  
+
 
 
 });
