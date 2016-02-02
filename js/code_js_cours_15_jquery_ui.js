e65046a7a513f26f667a8a9cf346489fca58a2f9
ChristopheMalo <christof.malo@gmail.com>
2016-02-02 18:28:35 +0100

    Coder exemples cours 15 jQuery UI -> accordion

--- js/code_js_cours_15_jquery_ui.js
+++ js/code_js_cours_15_jquery_ui.js
@@ -7,7 +7,10 @@ $(function() {
    // Déplacer les éléments
    $('#drag-div, #content-drag img').draggable();
    
-   // Redimensionner des éléments
+   // Redimensionner des éléments - utilisation d'un thème
    $('#contentresize').resizable();
    $( "#resizable" ).resizable();
+   
+   // Accordéon
+   $('#accordeon').accordion();
 });
